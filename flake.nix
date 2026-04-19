e8ed7815ce2ec0c28058550548936a78a9738dc2
srcres258 <src.res.211@gmail.com>
2026-04-19 22:53:33 +0800

    Add Nix flake
    Signed-off-by: srcres258 <src.res.211@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "NJU Digital Experiment - Verilog digital logic design with Verilator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      fhs = pkgs.buildFHSEnv {
+        name = "nju-digital-exp";
+        targetPkgs = pkgs: with pkgs; [
+          coreutils
+          findutils
+          verilator
+          gnumake
+          gcc
+          SDL2
+          SDL2_image
+          SDL2_ttf
+          python3
+          pkg-config
+        ];
+        runScript = "bash";
+        profile = ''
+          export NVBOARD_HOME="$PWD/nvboard"
+        '';
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ fhs ];
+        shellHook = ''
+          exec ${fhs}/bin/nju-digital-exp
+        '';
+      };
+    };
+}
